121c2afcb3cfa2557fe8c126d8a57a07bc0d4ffc
BackDoore <noempang.parattakorn@gmail.com>
2025-04-24 22:52:10 +0700

    ITCLASH Problems #1
    Committing TWO 0* Problems from ITCLASH'68 @KMITL

---
+++ ITCLASH/ไม่มีคำอธิบาย.py
@@ -0,0 +1,46 @@
+# ไม่มีคำอธิบาย (0*)
+
+# โจทย์ข้อนี้จะไม่มีมีคำอธิบายอะไรทั้งนั้นพี่อยากให้น้องๆดูที่เคสแล้วลองเขียนดูนะครับ :)
+
+
+#####################################
+
+# THIS DOESN'T WORK
+
+# ro1 = 1
+# ro2 = int(input()) + 1
+# r1 = ro1
+# r2 = ro2
+# reFINAL = (ro2 * 2)
+# re1 = ro1
+# re2 = ro2
+# i = 0
+# e = 0
+# n = 0
+# while re1 < reFINAL:
+#     while r1 < ro2:
+#         li = list(range(r1,r2))
+#         while i < len(li):
+#             if li[i] < 10:
+#                 li[i] = "{:02d}".format(li[i])
+#                 i = i+1
+#         i = 0
+#         print(*li)
+#         r1 = r1+1
+#         r2 = r2+1
+#     re1 = re1+1
+#     re2 = re2+1
+#     r1 = re1
+#     r2 = re2
+
+########################################
+
+# THIS ONE WORKS
+
+nigga = int(input())
+for a in range(1,nigga+1):
+    for i in range(a,nigga+a):
+        # print(i)
+        for j in range(i,nigga+i):
+            print("{:02d}".format(j), end=" ")
+        print()
