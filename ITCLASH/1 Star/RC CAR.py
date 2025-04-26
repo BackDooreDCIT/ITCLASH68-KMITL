07ff6fa78783adfaab29fb66c5b456c04f23356a
BackDoore <noempang.parattakorn@gmail.com>
2025-04-26 22:47:03 +0700

    ITCLASH Problems #5
    Finished solving RC CAR (1*)

--- ITCLASH/1 Star/RC CAR.py
+++ ITCLASH/1 Star/RC CAR.py
@@ -1,9 +1,35 @@
-minpos = 1
-maxpos = 25
-curpos = 0
-startpos = []
-allmap = []
-result = ""
+# RC CAR (1*)
+
+# พี่เพิ่งซื้อรถบังคับมาใหม่ราคา 1 แบงค์แดง (ทอน 0.25 บาท) ด้วยราคาไม่ถึง 1 แบงค์แดง คุณภาพก็อาจตามราคานั้นแหละ พี่อยากให้น้องๆ ช่วยตรวจสอบว่า รถบังคับของพี่อยู่ในพื้นที่ที่สามารถควบคุมได้ไหม?
+
+# โดยแทน 'o' เป็นตำแหน่งของรถบังคับ และ 'x' เป็นพื้นที่ว่างที่สามารถบังคับไปได้ รถบังคับสามารถบังคับได้แค่ 4 ทิศทางเท่านั้น:
+
+# U: ไปข้างหน้า (ขึ้น)
+
+# D: ไปข้างหลัง (ลง)
+
+# L: ไปทางซ้าย
+
+# R: ไปทางขวา
+
+# วิธีการทำงาน:
+# รับกริด 5x5 ที่ประกอบด้วยตัวอักษร 'o' (รถบังคับ) และ 'x' (พื้นที่ว่าง)
+
+# หลังจากนั้นให้ผู้ใช้กรอกคำสั่งเพื่อย้ายตำแหน่งของรถบังคับในกริด โดยใช้คำสั่ง:
+
+# 'U' (ขึ้น)
+
+# 'D' (ลง)
+
+# 'L' (ซ้าย)
+
+# 'R' (ขวา)
+
+# หากรถบังคับไปเกินขอบเขตกริด จะต้องแสดงข้อความว่า "Not in Area!!!" และจบการทำงานทันที
+
+# หากผู้ใช้กรอกคำสั่ง -1 จะต้องแสดงข้อความว่า "Stop" และจบการทำงานทันที
+
+
 checkforcar = ["o x x x x", "x o x x x", "x x o x x", "x x x o x", "x x x x o"]
 car1 = "o x x x x"
 car2 = "x o x x x"
@@ -11,212 +37,229 @@ car3 = "x x o x x"
 car4 = "x x x o x"
 car5 = "x x x x o"
 empty = "x x x x x"
-lastcmd = 1
-laststrcmd = ""
-lastval = 0
+allempty = ["x x x x x", "x x x x x", "x x x x x", "x x x x x", "x x x x x"]
+allmap = []
+startmap = []
+maxheight = ["ofb", "1", "2", "3", "4", "5", "ofb"]
+curheight = "ofb"
+maxwidth = ["ofb", "1", "2", "3", "4", "5", "ofb"]
+curwidth = "ofb"
+a = 0
+result = ""
 e = 0
 for i in range(5):
-    agar = input()
-    startpos.append(agar)
-    if agar == car1:
-        curpos = 1 + (5 * i)
-    elif agar == car2:
-        curpos = 2 + (5 * i)
-    elif agar == car3:
-        curpos = 3 + (5 * i)
-    elif agar == car4:
-        curpos = 4 + (5 * i)
-    elif agar == car5:
-        curpos = 5 + (5 * i)
-# print(curpos)
-# print(pos0)
-while curpos >= minpos and curpos <= maxpos:
-    lastval = lastval
-    cmd = input()
-    if lastcmd == 0:
-        allmap.append(empty)
-        allmap.append(empty)
-        allmap.append(empty)
-        allmap.append(empty)
-        allmap.append(empty)
+    mapinp = input().replace(" ", "")
+    mapinp = " ".join(mapinp)
+    startmap.append(mapinp)
+    if mapinp in checkforcar:
+        curheight = maxheight[i+1]
+        if mapinp == car1:
+            curwidth = maxwidth[1]
+        elif mapinp == car2:
+            curwidth = maxwidth[2]
+        elif mapinp == car3:
+            curwidth = maxwidth[3]
+        elif mapinp == car4:
+            curwidth = maxwidth[4]
+        elif mapinp == car5:
+            curwidth = maxwidth[5]
+# print(curheight)
+# print(curwidth)
+while a != -1:
+    if startmap == allempty:
+        result = "Not in Area!!!"
         break
+    if curheight == "ofb" or curwidth == "ofb":
+        allmap.append(empty)
+        allmap.append(empty)
+        allmap.append(empty)
+        allmap.append(empty)
+        allmap.append(empty)
+        result = "Not in Area!!!"
+        break
+    cmd = input()
     if cmd == "U":
-        curpos = curpos-5
+        curheight = maxheight[int(curheight)-1]
     elif cmd == "D":
-        curpos = curpos+5
+        curheight = maxheight[int(curheight)+1]
     elif cmd == "L":
-        curpos = curpos-1
+        curwidth = maxwidth[int(curwidth)-1]
     elif cmd == "R":
-        curpos = curpos+1
+        curwidth = maxwidth[int(curwidth)+1]
     elif cmd == "-1":
         result = "Stop"
         break
-    if curpos > 0 and curpos <= 5:
-        if curpos == 1:
-            if cmd == "L":
-                allmap.append(checkforcar[0])
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                result = "Not in Area!!!"
-                lastcmd = 0
-            elif cmd == "U":
-                allmap.append(checkforcar[curpos-1])
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                result = "Not in Area!!!"
-                lastcmd = 0
-        elif curpos == 5:
-            if cmd == "R":
-                allmap.append(checkforcar[4])
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                result = "Not in Area!!!"
-                lastcmd = 0
-            elif cmd == "U":
-                allmap.append(checkforcar[curpos-1])
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                result = "Not in Area!!!"
-                lastcmd = 0
-        else:
-            allmap.append(checkforcar[curpos-1])
+    if curheight == "1":
+        if curwidth == "1":
+            allmap.append(car1)
             allmap.append(empty)
             allmap.append(empty)
             allmap.append(empty)
             allmap.append(empty)
-    elif curpos > 5 and curpos <= 10:
-        if curpos == 6:
-            print('secondline')
-            if cmd == "L":
-                print('gethere')
-                allmap.append(empty)
-                allmap.append(checkforcar[0])
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                result = "Not in Area!!!"
-                lastcmd = 0
-        if curpos == 10:
-            if cmd == "R":
-                allmap.append(empty)
-                allmap.append(checkforcar[4])
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                result = "Not in Area!!!"
-                lastcmd = 0
-        else:
-            allmap.append(empty)
-            allmap.append(checkforcar[curpos-6])
-            allmap.append(empty)
-            allmap.append(empty)
-            allmap.append(empty)
-    elif curpos > 10 and curpos <= 15:
-        if curpos == 11:
-            if cmd == "L":
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(checkforcar[0])
-                allmap.append(empty)
-                allmap.append(empty)
-                result = "Not in Area!!!"
-                lastcmd = 0
-        if curpos == 15:
-            if cmd == "R":
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(checkforcar[4])
-                allmap.append(empty)
-                allmap.append(empty)
-                result = "Not in Area!!!"
-                lastcmd = 0
-        else:
-            allmap.append(empty)
-            allmap.append(empty)
-            allmap.append(checkforcar[curpos-11])
-            allmap.append(empty)
-            allmap.append(empty)
-    elif curpos > 15 and curpos <= 20:
-        if curpos == 16:
-            if cmd == "L":
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(checkforcar[0])
-                allmap.append(empty)
-                result = "Not in Area!!!"
-                lastcmd = 0
-        if curpos == 20:
-            if cmd == "R":
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(checkforcar[4])
-                allmap.append(empty)
-                result = "Not in Area!!!"
-                lastcmd = 0
-        else:
-            allmap.append(empty)
-            allmap.append(empty)
-            allmap.append(empty)
-            allmap.append(checkforcar[curpos-16])
-            allmap.append(empty)
-    elif curpos > 20 and curpos <= 25:
-        if curpos == 21:
-            if cmd == "D":
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(checkforcar[curpos-21])
-                result = "Not in Area!!!"
-                lastcmd = 0
-            elif cmd == "L":
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(checkforcar[0])
-                result = "Not in Area!!!"
-                lastcmd = 0
-        if curpos == 25:
-            if cmd == "D":
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(checkforcar[curpos-21])
-                result = "Not in Area!!!"
-                lastcmd = 0
-            elif cmd == "R":
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(empty)
-                allmap.append(checkforcar[4])
-                result = "Not in Area!!!"
-                lastcmd = 0
-        else:
+        elif curwidth == "2":
+            allmap.append(car2)
             allmap.append(empty)
             allmap.append(empty)
             allmap.append(empty)
             allmap.append(empty)
-            allmap.append(checkforcar[curpos-21])
-        laststrcmd = cmd
-        lastval = curpos
+        elif curwidth == "3":
+            allmap.append(car3)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+        elif curwidth == "4":
+            allmap.append(car4)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+        elif curwidth == "5":
+            allmap.append(car5)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+    elif curheight == "2":
+        if curwidth == "1":
+            allmap.append(empty)
+            allmap.append(car1)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+        elif curwidth == "2":
+            allmap.append(empty)
+            allmap.append(car2)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+        elif curwidth == "3":
+            allmap.append(empty)
+            allmap.append(car3)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+        elif curwidth == "4":
+            allmap.append(empty)
+            allmap.append(car4)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+        elif curwidth == "5":
+            allmap.append(empty)
+            allmap.append(car5)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+    elif curheight == "3":
+        if curwidth == "1":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car1)
+            allmap.append(empty)
+            allmap.append(empty)
+        elif curwidth == "2":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car2)
+            allmap.append(empty)
+            allmap.append(empty)
+        elif curwidth == "3":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car3)
+            allmap.append(empty)
+            allmap.append(empty)
+        elif curwidth == "4":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car4)
+            allmap.append(empty)
+            allmap.append(empty)
+        elif curwidth == "5":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car5)
+            allmap.append(empty)
+            allmap.append(empty)
+    elif curheight == "4":
+        if curwidth == "1":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car1)
+            allmap.append(empty)
+        elif curwidth == "2":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car2)
+            allmap.append(empty)
+        elif curwidth == "3":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car3)
+            allmap.append(empty)
+        elif curwidth == "4":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car4)
+            allmap.append(empty)
+        elif curwidth == "5":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car5)
+            allmap.append(empty)
+    elif curheight == "5":
+        if curwidth == "1":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car1)
+        elif curwidth == "2":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car2)
+        elif curwidth == "3":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car3)
+        elif curwidth == "4":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car4)
+        elif curwidth == "5":
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(empty)
+            allmap.append(car5)
 length = len(allmap)
-print(length)
+# print(length)
 for i in range(int(length/5)):
     for a in range(5):
         print(allmap[e])
         e = e+1
     print()
 print(result)
+
+# NOTE: This is not a very efficient way of solving this problem. Instead, you can use .find() or .index()
+#       to make your life easier :P
+
+
+                            # &   Made by Parattakorn Boonprakob :P   & #
+                        # !!! IF YOU USE THIS CODE, PLEASE CREDIT THE ORIGINAL !!! #
+                                    # ???   Thanks for reading =Δ=   ??? #
+
+                                            # ~ From the team, ~ #
+                                         #  ^「The Procasinators」^  #
