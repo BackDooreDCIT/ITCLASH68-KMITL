2d76bc714f8c6876f0fd1f4aab0a087c081def5e
BackDoore <noempang.parattakorn@gmail.com>
2025-05-05 19:26:42 +0700

    BIGGEST ISLAND Progression
    Making some progress on BIGGEST ISLAND (3*)

--- ITCLASH/3 Stars/BIGGEST ISLAND.py
+++ ITCLASH/3 Stars/BIGGEST ISLAND.py
@@ -1,9 +1,20 @@
 mapsize = input().split() # mapsize
 seamap = []
 islandsize = 0
+findland = 0
+landcount = 0
+biggestislandsize = 0
 # print(size)
 for i in range(int(mapsize[0])): # map input
     lineinp = input().replace(" ", "")
     seamap.append(lineinp)
-# how 2 find islands
+for i in range(len(seamap)): # checking if island exist
+    x = seamap[i].find("1")
+    if x == -1:
+        findland = 0
+    else:
+        findland += 1
+        landcount = seamap[i].count("1")
+
+print(biggestislandsize)
 # 
